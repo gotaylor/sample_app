445a7dd1e6f4bc3ce7ff9117e1f59c2d0230e69c
Gordon Taylor <gordeau1@gmail.com>
2013-01-03 17:45:29 -0600

    Finish layout and routes

---
+++ ffi/ruby/1.9.1/gems/jquery-rails-2.1.0/lib/jquery/rails/version.rb
@@ -0,0 +1,8 @@
+module Jquery
+  module Rails
+    VERSION = "2.1.0"
+    JQUERY_VERSION = "1.8.0"
+    JQUERY_UI_VERSION  = "1.8.23"
+    JQUERY_UJS_VERSION = "12da9fc2f175c8e445413b15cf6b685deb271d6e"
+  end
+end
