fbe18c4d42373d76e1b2639c4e8054df0fae5edb
Gordon Taylor <gordeau1@gmail.com>
2013-01-21 10:15:09 -0600

    Fix bug in Sign out link test after successful account creation and auto sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -55,13 +55,13 @@ describe "User Pages" do
   		end
 
   		describe "after saving a user" do
-
   			before { click_button submit }
 
   			let(:user) { User.find_by_email("user@example.com") }
   			
-  			it {should have_selector('title', text: user.name) }
+  			it { should have_selector('title', text: user.name) }
   			it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link ('Sign out') }
   		end
   	end
   end
